45b92dbee4c3fdad42feacd9242d54aeb16e9c5c
DURAND <junior_durand@outlook.com>
2019-04-19 16:19:23 -0300

    criando aplicação para testar novos métodos.

---
+++ collection/apl3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "colecao.h"
+
+typedef struct _time_{
+	char nome[20];
+	int numCampeao;
+	int numVice;
+}Time;
+
+int main(void){
+	Col* c; Time* t; Time ta;
+	c = colCreate(10);
+	if(c != NULL){
+		for(int i=0; i<4; i++){
+			t=(Time*)malloc(sizeof(Time));
+			if(t != NULL){
+				scanf("%s %d %d",(t->nome) , &(t->numCampeao), &t->numVice);
+				colInsert(c, (void*)t);
+			}
+		}
+		printf("\n---------------\n");
+		t = (Time*)colQueryFirst(c);
+		while(t != NULL){
+			printf("%s %d %d\n",t->nome , t->numCampeao, t->numVice);
+			t = (Time*)colQueryNext(c);
+		}
+		printf("\n---------------\n");
+		t = colRemoveFirst(c);
+		printf("%s %d %d\n",t->nome , t->numCampeao, t->numVice);
+		
+		t = (Time*)colQueryFirst(c);
+		while(t != NULL){
+			printf("%s %d %d\n",t->nome , t->numCampeao, t->numVice);
+			t = (Time*)colQueryNext(c);
+		}
+		printf("\n---------------\n");
+	}
+	return 0; 
+}
