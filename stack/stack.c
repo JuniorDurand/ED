35029e58c8667c57eb5572e150b851b731ea057f
DURAND <junior_durand@outlook.com>
2019-05-01 22:55:32 -0300

    implementação de pilha estática completa.

---
+++ stack/stack.c
@@ -0,0 +1,58 @@
+#define _STACK_C_
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+#define TRUE 1
+#define FALSE 0
+
+Stack* stkCreate(int max){
+	Stack* s;
+	if(max>0){
+		s = (Stack*)malloc(sizeof(Stack));
+		if(s!=NULL){
+			s->elms = (void**)malloc(sizeof(void*)*max);
+			if (s->elms !=NULL){
+				s->max = max;
+				s->top = -1;
+
+				return s;
+			}else{
+				free(s);
+			}
+		}
+	}
+	return NULL;
+}
+
+int stkDestroy(Stack *s){
+	if(s != NULL){
+		if(s->top < 0){
+			free(s->elms);
+			free(s);
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+
+int stkPush(Stack* s, void* elm){
+	if(s != NULL){
+		if(s->top < s->max-1){
+			s->top++;
+			s->elms[s->top] = elm;
+			return TRUE;
+		}
+	}
+	return FALSE
+}
+
+void* stkPop(Stack* s){
+	if(s != NULL){
+		if(s->top<0){
+			void* elm = s->elms[s->top];
+			s->top--;
+			return elm;
+		}
+	}
+	return NULL;
+}
---
+++ stack/stack.h
@@ -0,0 +1,29 @@
+#ifndef _STACK_H_
+#define _STACK_H_
+
+	#ifdef _STACK_C_
+		typedef struct _stack_{
+			int max;
+			int top;
+			int cur;
+			void* *elms;//vetor de void* de tamanho max
+		}Stack;
+
+		Stack* stkCreate(int max);
+		int stkDestroy(Stack *s);
+		int stkPush(Stack* s, void* elm);
+		void* stkPop(Stack* s);
+
+	#else
+		
+		typedef struct _stack_ Stack;
+		extern Stack* stkCreate(int max);
+		extern int stkDestroy(Stack *s);
+		extern int stkPush(Stack* s, void* elm);
+		extern void* stkPop(Stack* s);
+
+
+
+	#endif // _STACK_C_
+
+#endif
