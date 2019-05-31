1f7141f2ff825449afaddcd27bb5916889ca84f0
DURAND <junior_durand@outlook.com>
2019-05-30 21:56:15 -0300

    Iniciando implementação de lista circular simplesmente encadeada.

---
+++ SLCircularList/slclist.h
@@ -0,0 +1,28 @@
+#ifndef _SLCLIST_H_
+#define _SLCLIST_H_
+
+	#ifdef _SLLIST_C_
+		typedef struct _sllelm_{
+			struct _sllelm_ *next;
+			void* data;
+		}SLNode;
+
+		typedef struct _sllist_{
+			SLNode* first;
+			SLNode* cur;
+		}SLList;
+		
+		SLList* sllCreate();
+		int sllDestroy(sllist* list);
+
+
+	#else		
+		typedef struct _sllelm_ SLNode;
+		typedef struct _sllist_ SLList;
+
+		SLList* sllCreate();
+		int sllDestroy(sllist* list);
+
+	#endif // _SLCLIST_C_
+
+#endif
