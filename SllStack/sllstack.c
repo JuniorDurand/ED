539bfcf5bf6a511a76a828f5fa6c27291ecea8e5
DURAND <junior_durand@outlook.com>
2019-07-04 09:18:20 -0300

    Terminando implementação de pilha baseada em SSList.

--- SllStack/sllstack.c
+++ SllStack/sllstack.c
@@ -4,3 +4,76 @@
 #include "sllstack.h"
 #define TRUE 1
 #define FALSE 0
+
+SLList* sllStkCreate(){
+	SLList* list;
+	list = (SLList*)malloc(sizeof(SLList));
+	if(list != NULL){
+		list->first = NULL;
+		list->cur = NULL;
+		return list;
+	}
+	return NULL;
+}
+
+int sllStkDestroy(sllist* list){
+	if(list != NULL){
+		if(list->first == NULL){
+			free(list);
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+
+int sllStkPush(SLList *list, void* data){
+	SLNode* newNode;
+	if(list!=NULL){
+		newNode = (SLNode*)malloc(sizeof(SLNode));
+		if(newNode != NULL){
+			newNode->data = data;
+			newNode->next = list->first;
+			list->first = newNode;
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+
+void* sllStkPop(SLList* list){
+	SLNode* aux;
+	void* data;
+	if(list != NULL){
+		aux= list->first;
+		list->first = aux->next;
+		data = aux->data;
+		free(aux);
+		return data;
+	}
+	return NULL;
+}
+
+
+void* sllStkGetFirst(SLList *list){
+	void* data;
+	if(list != NULL){
+		if(list->first != NULL){
+			list->cur = list->first;
+			data = list->first->data;
+			return data;
+		}
+	}
+	return NULL;
+}
+
+void* sllStkGetNext(SLList* list){
+	void* data;
+	if(list != NULL){
+		if(list->cur != NULL){
+			list->cur = list->cur->next;
+			data = list->cur->data;
+			return data;
+		}
+	}
+	return NULL;
+}
--- SllStack/sllstack.h
+++ SllStack/sllstack.h
@@ -9,14 +9,26 @@
 
 		typedef struct _sllstack_{
 			SLNode* first;
+			SLNode* cur;
 			void* data;
 		}SLLStack;
-		
+		SLList* sllStkCreate();
+		int sllStkDestroy(sllist* list);
+		int sllStkPush(SLList *list, void* data);
+		void* sllStkPop(SLList* list);
+		void* sllStkGetFirst(SLList *list);
+		void* sllStkGetNext(SLList* list);
 
 
 	#else		
 		typedef struct _sllelm_ SLNode;
 		typedef struct _sllist_ SLList;
+		SLList* sllStkCreate();
+		int sllStkDestroy(sllist* list);
+		int sllStkPush(SLList *list, void* data);
+		void* sllStkPop(SLList* list);
+		void* sllStkGetFirst(SLList *list);
+		void* sllStkGetNext(SLList* list);
 
 
 	#endif // _SLLSTACK_C_
